29014baa4b8f71c36b4ac755661524ff7db5ae1d
KoyFoster <koyyashina@hotmail.com>
2022-06-26 12:56:02 -0500

    Phaser3 and Electron Setup

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,44 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Phaser from 'phaser';
+import logoImg from './assets/logo.png';
+
+class MyGame extends Phaser.Scene
+{
+    preload ()
+    {
+        this.load.image('logo', logoImg);
+    }
+
+    create ()
+    {
+        const logo = this.add.image(400, 150, 'logo');
+
+        // bounce
+        this.tweens.add({
+            targets: logo,
+            y: 450,
+            duration: 2000,
+            ease: "Power2",
+            yoyo: true,
+            loop: -1
+        });
+    }
+}
+
+// Game Window Config
+const config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 1920,
+    height: 1080,
+    scene: MyGame
+};
+
+const game = new Phaser.Game(config);
+
+// Main app
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
@@ -12,8 +50,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
