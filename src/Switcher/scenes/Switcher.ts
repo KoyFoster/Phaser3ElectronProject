5bb1cb3ebf1108d2acf526d5a54f4e93e6ffc491
KoyFoster <koyyashina@hotmail.com>
2022-07-08 19:10:17 -0500

    Moved Switcher to it's on file

---
+++ src/Switcher/scenes/Switcher.ts
@@ -0,0 +1,142 @@
+import { Attack } from "../components/Attack";
+import { Damage } from "../components/Damage";
+import { PlayerMovement } from "../components/PlayerMovement";
+import IComponentService from "../services/ComponentService";
+
+function spawnAroundFrame(
+  width: number,
+  height: number,
+  group: Phaser.Physics.Arcade.Group,
+  sprite: string,
+  num: number,
+  padding = 500,
+  components: IComponentService
+) {
+  // spawn right outside of view
+  for (let i = 0; i < num; i += 1) {
+    const x = Math.floor(Math.random() * width);
+    const y = Math.floor(Math.random() * height);
+    const i = Math.random() > 0.5;
+    const mob = group.create(
+      x > y ? x - padding : i ? width + padding : 0,
+      x > y ? (i ? height + padding : 0) : y - padding,
+      sprite
+    ) as Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+    const comp = new Damage();
+    components.addComponent(mob, comp);
+    // components.removeComponent(mob, comp);
+    mob.setVelocity(240, 0);
+    mob.setVelocity(240, 0);
+    mob.setFriction(1);
+  }
+}
+
+export class Switcher extends Phaser.Scene {
+  private components!: IComponentService;
+  boundaries!: Phaser.Physics.Arcade.StaticGroup;
+  player!: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+  mobs!: Phaser.Physics.Arcade.Group;
+  cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
+  keys: any;
+
+  score = 0;
+  debugText!: Phaser.GameObjects.Text;
+  gameOver = false;
+
+  constructor() {
+    super("Switcher");
+  }
+
+  preload() {}
+
+  followPlayer() {
+    // chase player
+    this.mobs.children.iterate((mob) => {
+      this.physics.moveToObject(mob, this.player, 120);
+    });
+  }
+
+  init() {
+    this.components = new IComponentService();
+    this.events.on(Phaser.Scenes.Events.SHUTDOWN, () => {
+      this.components.destroy();
+    });
+  }
+
+  create() {
+    const { width, height } = this.sys.game.canvas;
+
+    // boundaries
+    this.boundaries = this.physics.add.staticGroup();
+    const tile = this.add.tileSprite(
+      width * 0.5,
+      height * 0.5,
+      width,
+      height,
+      "tile0"
+    );
+    // frame
+    this.boundaries
+      .create(width * 0.5, 0, "pixel")
+      .setScale(width, 1)
+      .refreshBody(); // Top
+    this.boundaries
+      .create(width * 0.5, height, "pixel")
+      .setScale(width, 1)
+      .refreshBody(); // Bottom
+    this.boundaries
+      .create(0, height * 0.5, "pixel")
+      .setScale(1, height)
+      .refreshBody(); // Left
+    this.boundaries
+      .create(width, height * 0.5, "pixel")
+      .setScale(1, height)
+      .refreshBody(); // Right
+
+    // listeners
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.keys = this.input.keyboard.addKeys("W,A,S,D");
+
+    // score
+    this.debugText = this.add.text(16, 16, "debugText: 0", {
+      fontSize: "32px",
+      color: "#eeee00",
+    });
+
+    this.player = this.physics.add.sprite(width * 0.5, height * 0.5, "dude");
+    this.player.setDrag(0.0001);
+    this.player.setDamping(true);
+
+    this.cameras.main.startFollow(this.player);
+
+    // Spawn some mobs
+    this.mobs = this.physics.add.group();
+    spawnAroundFrame(width, height, this.mobs, "bomb", 10, 0, this.components);
+
+    // Add movement to player
+    this.components.addComponent(
+      this.player,
+      new PlayerMovement(this.cursors, this.keys)
+    );
+
+    // Set new attack
+    const newAttack = new Attack("upswing", "swipe");
+    this.components.addComponent(this.player, newAttack);
+    newAttack.setMobs(this.mobs as Phaser.Physics.Arcade.Group);
+    newAttack.setInput(() => {
+      return this.cursors.space.isDown;
+    });
+
+    // collisions
+    this.physics.add.collider(this.mobs, this.boundaries);
+    this.physics.add.collider(this.player, this.boundaries);
+  }
+
+  update(t: number, dt: number) {
+    this.debugText.text = `Vel: [${this.player.body.velocity.x}, ${this.player.body.velocity.y}],
+      Acc: [${this.player.body.acceleration.x}, ${this.player.body.acceleration.y}]`;
+
+    this.followPlayer();
+    this.components.update(dt);
+  }
+}
--- src/Switcher/scenes/preloader.ts
+++ src/Switcher/scenes/preloader.ts
@@ -1,4 +1,4 @@
-import Phaser from 'phaser'
+import Phaser from "phaser";
 import ground from "../../assets/platform.png";
 import boundary from "../../assets/boundary.png";
 import tile0 from "../../assets/tile0.png";
@@ -16,41 +16,69 @@ import swipe from "../../assets/audio/fx/swipe.mp3";
 import punch from "../../assets/audio/fx/punch.mp3";
 import slap from "../../assets/audio/fx/slap.mp3";
 
+export default class Preloader extends Phaser.Scene {
+  constructor() {
+    super("preloader");
+  }
 
-export default class Preloader extends Phaser.Scene
-{
-	constructor()
-	{
-		super('preloader')
-	}
+  preload() {
+    // load image assets
+    this.load.audio("swipe", swipe);
+    this.load.audio("punch", punch);
+    this.load.audio("slap", slap);
 
-	preload()
-	{
-		// load image assets
-		this.load.audio('swipe', swipe);
-		this.load.audio('punch', punch);
-		this.load.audio('slap', slap);
+    // load image assets
+    this.load.image("upswing", upswing);
+    this.load.image("pixel", pixel);
+    this.load.image("star", star);
+    this.load.image("bomb", bomb);
+    this.load.image("ground", ground);
+    this.load.image("boundary", boundary);
+    this.load.image("tile0", tile0);
+    this.load.image("hurbox", hurbox);
+    this.load.image("logoImg", logoImg);
+    this.load.image("sky", sky);
+    this.load.spritesheet("dude", dude, {
+      frameWidth: 32,
+      frameHeight: 48,
+    });
+  }
 
-		// load image assets
-		this.load.image("upswing", upswing);
-		this.load.image("pixel", pixel);
-		this.load.image("star", star);
-		this.load.image("bomb", bomb);
-		this.load.image("ground", ground);
-		this.load.image("boundary", boundary);
-		this.load.image("tile0", tile0);
-		this.load.image("hurbox", hurbox);
-		this.load.image("logoImg", logoImg);
-		this.load.image("sky", sky);
-		this.load.spritesheet("dude", dude, {
-		  frameWidth: 32,
-		  frameHeight: 48,
-		});
-	}
+  create() {
+    this.add.text(20, 20, "Loading assets...", {
+      fontSize: "32px",
+      color: "#FFFF00",
+    });
 
-	create()
-	{
-        this.add.text(20, 20, "Loading assets...", { fontSize: '32px', color: '#FFFF00' });
-		this.scene.start('Switcher')
-	}
+    // Generate animations for player
+    this.anims.create({
+		key: "left",
+		frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+		frameRate: 10,
+		repeat: -1,
+	  });
+	  this.anims.create({
+		key: "turn",
+		frames: [{ key: "dude", frame: 4 }],
+		frameRate: 20,
+	  });
+	  this.anims.create({
+		key: "down",
+		frames: [{ key: "dude", frame: 4 }],
+		frameRate: 20,
+	  });
+	  this.anims.create({
+		key: "up",
+		frames: [{ key: "dude", frame: 5 }],
+		frameRate: 20,
+	  });
+	  this.anims.create({
+		key: "right",
+		frames: this.anims.generateFrameNumbers("dude", { start: 6, end: 9 }),
+		frameRate: 10,
+		repeat: -1,
+	  });
+
+    this.scene.start("Switcher");
+  }
 }
